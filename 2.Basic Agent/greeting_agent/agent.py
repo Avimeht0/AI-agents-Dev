367ee3385935324f0eba0f9b9fbe5f7fb73d61b9
Arvind Metha <ar.kumar9918@gmail.com>
2025-09-17 11:37:36 +0530

    updated basic agent

--- 2.Basic Agent/greeting_agent/agent.py
+++ 2.Basic Agent/greeting_agent/agent.py
@@ -1,9 +1,16 @@
 from google.adk import Agent
 from google.adk.models.lite_llm import LiteLlm
+import os
+from dotenv import load_dotenv
+load_dotenv()
+model = LiteLlm(model="openrouter/mistralai/mistral-small-3.2-24b-instruct:free",
+               api_key=os.getenv("OPENROUTER_API_KEY"))
+
+
 
 root_agent=Agent(
     name="greeting_agent",
-    model=LiteLlm(model="gpt-4o-mini"),
+    model=model,
     description="A greeting agent that greets the user",
     instruction="You are a greeting agent that greets the user",
      
